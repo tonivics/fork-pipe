d32f97f7e17fdd033048cf46b09e83e373e24343
Antonio Vicente <38213571+tonivics@users.noreply.github.com>
2022-08-03 11:09:53 -0300

    Add files via upload

--- pipe_02.c
+++ pipe_02.c
@@ -1,3 +1,5 @@
+// Link de referência: https://www.programacaoprogressiva.net/2014/09/Pipes-em-C-Comunicao-entre-Processos-IPC-Interprocess-Communication.html
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <sys/types.h>
@@ -6,7 +8,6 @@
 int main(void){
 	int file_descriptor[2];
 	pid_t pid;
-	int soma_pares, soma_impares, soma_total;
 	
 	if(pipe(file_descriptor)<0){
 		perror("pipe");
@@ -20,7 +21,7 @@ int main(void){
 	
 	if(pid==0){
 		close(file_descriptor[0]);
-		
+		int soma_pares;
 		soma_pares = 2+4+6+8+10;
 		write(file_descriptor[1], &soma_pares, sizeof(soma_pares));
 		
@@ -28,13 +29,16 @@ int main(void){
 		exit(0);
 	}else{
 		close(file_descriptor[1]);
-		
+		int pares;
+		read(file_descriptor[0], &pares, sizeof(pares));
+		int soma_impares;
 		soma_impares = 1+3+5+7+9;
-		soma_total = soma_pares + soma_impares;
-		read(file_descriptor[0], &soma_total, sizeof(soma_total));
+		int soma_total;
+		soma_total = pares + soma_impares;
 		
 		printf("PID: %d :: Soma dos ímpares: %d\n", pid, soma_impares);
 		printf("PID: %d :: Soma total: %d\n", pid, soma_total);
 		exit(0);
 	}
+	
 }
